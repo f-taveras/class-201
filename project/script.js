3669cfdb9c1fd6b8786b4d827b18385ab90c784f
Felix A. Taveras <felixataveras1@gmail.com>
2023-10-24 06:08:34 -0400

    saving

--- project/script.js
+++ project/script.js
@@ -2,6 +2,6 @@
 let Q1 = prompt("Identify yourself! You will have to solve a number of puzzles to get through");
 alert(Q1 + "... I guess that will do");
 
-    let Q2 = prompt('Type a color');
-    alert(Q2 + " is ok, I like charcoal");
-    document.body.innerHTML += `<p class="q1" style="color: ${Q2.toLowerCase()}">Thank you ${Q1}, is ${Q2} your color?</p>`
+let Q2 = prompt('Type a color');
+alert(Q2 + " is ok, I like charcoal");
+document.body.innerHTML += `<p class="q1" style="color: ${Q2.toLowerCase()}">Thank you ${Q1}, is ${Q2} your color?</p>`
