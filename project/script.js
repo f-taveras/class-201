5a5d8d8660281ddfd909321ef0b75b13aeb2e738
Felix A. Taveras <felixataveras1@gmail.com>
2023-10-24 07:15:40 -0400

    lab 2 completed

--- project/script.js
+++ project/script.js
@@ -1,7 +1,18 @@
-
-let Q1 = prompt("Identify yourself! You will have to solve a number of puzzles to get through");
+let Q1 = prompt(
+  "Identify yourself! You will have to solve a number of puzzles to get through"
+);
 alert(Q1 + "... I guess that will do");
 
-let Q2 = prompt('Type a color');
-alert(Q2 + " is ok, I like charcoal");
-document.body.innerHTML += `<p class="q1" style="color: ${Q2.toLowerCase()}">Thank you ${Q1}, is ${Q2} your color?</p>`
+let Q2 = prompt("What's your favorite color");
+if (Q2 === "blue") {
+  alert("Oh wow! That's my favorite color");
+} else alert(Q2 + " is ok, I like blue");
+document.body.innerHTML += `<p style="color: ${Q2.toLowerCase()}">Thank you ${Q1}, is ${Q2} your color?</p>`;
+
+let userAnswer = prompt("Would you like to know more about me?");
+while (userAnswer !== "yes") {
+  alert("You should try that again");
+  userAnswer = prompt("Would you like to know more about me?");
+}
+
+let myName = prompt ("You can come in, guess my name")
